ea59ae786cd424c7bbb33e50a26206e909652d70
ishwari <ishwariruikar@gmail.com>
2025-07-30 05:19:46 +0000

    first commit

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
-public class myfile {
+public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello from Jenkins!");
     }
 }
+
